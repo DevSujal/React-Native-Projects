3dd0fcd2c2dbcd6694800e16ae97517edcc869d1
DevSujal <nimjesujal2004@gmail.com>
2024-06-28 15:52:19 +0530

    "Added ActionCard and ContactList components to App.tsx"

--- styleinreact/App.tsx
+++ styleinreact/App.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 import FlatCard from './components/FlatCard';
 import ElevetedCard from './components/ElevetedCard';
 import FancyCard from './components/FancyCard';
+import ActionCard from './components/ActionCard';
+import ContactList from './components/ContactList';
 
 const App = () => {
   return (
@@ -11,6 +13,8 @@ const App = () => {
         <FlatCard />
         <ElevetedCard />
         <FancyCard />
+        <ActionCard />
+        <ContactList />
       </ScrollView>
     </SafeAreaView>
   );
